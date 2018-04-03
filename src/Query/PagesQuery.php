971a4b416929a9d5e7ae34966b2bad51b9203ed6
Ary Wibowo <nucreativa@gmail.com>
2018-04-03 20:15:27 +0700

    Update PagesQuery

--- src/Query/PagesQuery.php
+++ src/Query/PagesQuery.php
@@ -7,6 +7,7 @@ use GraphQL;
 use GraphQL\Type\Definition\Type;
 use Folklore\GraphQL\Support\Query;
 use LaravelWordpressModels\Models\Post;
+use GraphQL\Type\Definition\ResolveInfo;
 
 class PagesQuery extends Query {
 	protected $attributes = [
@@ -19,14 +20,35 @@ class PagesQuery extends Query {
 
 	public function args() {
 		return [
-			'id'     => [ 'name' => 'id', 'type' => Type::string() ],
-			'slug'   => [ 'name' => 'slug', 'type' => Type::string() ],
-			'status' => [ 'name' => 'status', 'type' => GraphQL::type( 'PostStatus' ) ]
+			'id'         => [ 'name' => 'id', 'type' => Type::string() ],
+			'slug'       => [ 'name' => 'slug', 'type' => Type::string() ],
+			'status'     => [ 'name' => 'status', 'type' => GraphQL::type( 'PostStatus' ) ],
+			'categories' => [ 'name' => 'categories', 'type' => Type::string() ]
 		];
 	}
 
-	public function resolve( $root, $args = [] ) {
-		$post = Post::on('wordpress')->type( 'page' );
+	public function resolve( $root, $args = [], $context, ResolveInfo $info ) {
+		$fields = $info->getFieldSelection( $depth = 3 );
+
+		$post = Post::on( 'wordpress' )->type( 'page' );
+
+		foreach ( $fields as $field => $keys ) {
+			switch ( $field ) {
+				case 'categories':
+					$post->with( 'categories' );
+					break;
+				case 'tags':
+					$post->with( 'tags' );
+					break;
+				case 'comments':
+					$post->with( 'comments' );
+					break;
+				case 'attachments':
+					$post->with( 'attachments' );
+					break;
+			}
+		}
+
 		if ( isset( $args['status'] ) ) {
 			$post = $post->where( 'post_status', $args['status'] );
 		}
@@ -34,6 +56,12 @@ class PagesQuery extends Query {
 			$post = $post->where( 'ID', $args['id'] );
 		} else if ( isset( $args['slug'] ) ) {
 			$post = $post->where( 'post_name', $args['slug'] );
+		} else if ( isset( $args['categories'] ) ) {
+			$post = $post->join( 'term_relationships', 'posts.id', '=', 'term_relationships.object_id' )
+			             ->join( 'term_taxonomy', 'term_relationships.term_taxonomy_id', '=', 'term_taxonomy.term_id' )
+			             ->join( 'terms', 'terms.term_id', '=', 'term_taxonomy.term_id' )
+			             ->where( 'term_taxonomy.taxonomy', '=', 'category' )
+			             ->where( 'terms.slug', '=', $args['categories'] );
 		}
 
 		return $post->get();
